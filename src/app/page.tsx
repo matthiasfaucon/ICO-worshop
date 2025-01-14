449131281da191385b04e7fa18b8b3d3f525453b
matthias <matthias.faucon@orange.fr>
2025-01-14 22:54:01 +0100

    fix: add bcrypt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@
 
 import ConnectedHome from "@/components/ConnectedHome";
 import HomePage from "@/components/Homepage";
-import RulesSlider from "./components/RulesSlider";
+import RulesSlider from "@/components/RulesSlider";
 
 export default function Home() {
   return <ConnectedHome />;
-
 }
